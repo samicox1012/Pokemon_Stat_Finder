9990d38778a19adc69678e06375f34e5c1f6123b
Sami Cox <samicox1012@gmail.com>
2016-05-30 12:20:11 -0500

    added search by box and menu strip

---
+++ pokeStatFinder/PokeStatFinder/Stat Finder.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace PokeStatFinder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.printToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.emailToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorThemesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lightToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.darkToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addNatureToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addAbilityToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutStatFinderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.searchByGroupBox = new System.Windows.Forms.GroupBox();
+            this.searchByNameLabel = new System.Windows.Forms.Label();
+            this.searchByNumberLabel = new System.Windows.Forms.Label();
+            this.numberComboBox = new System.Windows.Forms.ComboBox();
+            this.nameComboBox = new System.Windows.Forms.ComboBox();
+            this.menuStrip1.SuspendLayout();
+            this.searchByGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.toolsToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(868, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.printToolStripMenuItem,
+            this.emailToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // printToolStripMenuItem
+            // 
+            this.printToolStripMenuItem.Name = "printToolStripMenuItem";
+            this.printToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.P)));
+            this.printToolStripMenuItem.Size = new System.Drawing.Size(143, 22);
+            this.printToolStripMenuItem.Text = "Print";
+            // 
+            // emailToolStripMenuItem
+            // 
+            this.emailToolStripMenuItem.Name = "emailToolStripMenuItem";
+            this.emailToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.E)));
+            this.emailToolStripMenuItem.Size = new System.Drawing.Size(143, 22);
+            this.emailToolStripMenuItem.Text = "Email";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.colorThemesToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // colorThemesToolStripMenuItem
+            // 
+            this.colorThemesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lightToolStripMenuItem,
+            this.darkToolStripMenuItem});
+            this.colorThemesToolStripMenuItem.Name = "colorThemesToolStripMenuItem";
+            this.colorThemesToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.colorThemesToolStripMenuItem.Text = "Color Themes";
+            // 
+            // lightToolStripMenuItem
+            // 
+            this.lightToolStripMenuItem.Name = "lightToolStripMenuItem";
+            this.lightToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.L)));
+            this.lightToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
+            this.lightToolStripMenuItem.Text = "Light";
+            // 
+            // darkToolStripMenuItem
+            // 
+            this.darkToolStripMenuItem.Name = "darkToolStripMenuItem";
+            this.darkToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.D)));
+            this.darkToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
+            this.darkToolStripMenuItem.Text = "Dark";
+            // 
+            // toolsToolStripMenuItem
+            // 
+            this.toolsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addNatureToolStripMenuItem,
+            this.addAbilityToolStripMenuItem});
+            this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+            this.toolsToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.toolsToolStripMenuItem.Text = "Tools";
+            // 
+            // addNatureToolStripMenuItem
+            // 
+            this.addNatureToolStripMenuItem.Name = "addNatureToolStripMenuItem";
+            this.addNatureToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.addNatureToolStripMenuItem.Size = new System.Drawing.Size(178, 22);
+            this.addNatureToolStripMenuItem.Text = "Add Nature";
+            // 
+            // addAbilityToolStripMenuItem
+            // 
+            this.addAbilityToolStripMenuItem.Name = "addAbilityToolStripMenuItem";
+            this.addAbilityToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.A)));
+            this.addAbilityToolStripMenuItem.Size = new System.Drawing.Size(178, 22);
+            this.addAbilityToolStripMenuItem.Text = "Add Ability";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helpToolStripMenuItem,
+            this.aboutStatFinderToolStripMenuItem});
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.H)));
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(206, 22);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutStatFinderToolStripMenuItem
+            // 
+            this.aboutStatFinderToolStripMenuItem.Name = "aboutStatFinderToolStripMenuItem";
+            this.aboutStatFinderToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.aboutStatFinderToolStripMenuItem.Size = new System.Drawing.Size(206, 22);
+            this.aboutStatFinderToolStripMenuItem.Text = "About Stat Finder";
+            // 
+            // searchByGroupBox
+            // 
+            this.searchByGroupBox.Controls.Add(this.nameComboBox);
+            this.searchByGroupBox.Controls.Add(this.numberComboBox);
+            this.searchByGroupBox.Controls.Add(this.searchByNumberLabel);
+            this.searchByGroupBox.Controls.Add(this.searchByNameLabel);
+            this.searchByGroupBox.Location = new System.Drawing.Point(12, 46);
+            this.searchByGroupBox.Name = "searchByGroupBox";
+            this.searchByGroupBox.Size = new System.Drawing.Size(268, 102);
+            this.searchByGroupBox.TabIndex = 1;
+            this.searchByGroupBox.TabStop = false;
+            this.searchByGroupBox.Text = "Search By";
+            // 
+            // searchByNameLabel
+            // 
+            this.searchByNameLabel.AutoSize = true;
+            this.searchByNameLabel.Location = new System.Drawing.Point(16, 28);
+            this.searchByNameLabel.Name = "searchByNameLabel";
+            this.searchByNameLabel.Size = new System.Drawing.Size(38, 13);
+            this.searchByNameLabel.TabIndex = 0;
+            this.searchByNameLabel.Text = "Name:";
+            // 
+            // searchByNumberLabel
+            // 
+            this.searchByNumberLabel.AutoSize = true;
+            this.searchByNumberLabel.Location = new System.Drawing.Point(7, 63);
+            this.searchByNumberLabel.Name = "searchByNumberLabel";
+            this.searchByNumberLabel.Size = new System.Drawing.Size(47, 13);
+            this.searchByNumberLabel.TabIndex = 1;
+            this.searchByNumberLabel.Text = "Number:";
+            // 
+            // numberComboBox
+            // 
+            this.numberComboBox.FormattingEnabled = true;
+            this.numberComboBox.Location = new System.Drawing.Point(60, 60);
+            this.numberComboBox.Name = "numberComboBox";
+            this.numberComboBox.Size = new System.Drawing.Size(46, 21);
+            this.numberComboBox.TabIndex = 2;
+            // 
+            // nameComboBox
+            // 
+            this.nameComboBox.FormattingEnabled = true;
+            this.nameComboBox.Location = new System.Drawing.Point(60, 25);
+            this.nameComboBox.Name = "nameComboBox";
+            this.nameComboBox.Size = new System.Drawing.Size(178, 21);
+            this.nameComboBox.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(868, 436);
+            this.Controls.Add(this.searchByGroupBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Pokemon Stat Finder";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.searchByGroupBox.ResumeLayout(false);
+            this.searchByGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem printToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem emailToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem colorThemesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lightToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem darkToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addNatureToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addAbilityToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutStatFinderToolStripMenuItem;
+        private System.Windows.Forms.GroupBox searchByGroupBox;
+        private System.Windows.Forms.ComboBox nameComboBox;
+        private System.Windows.Forms.ComboBox numberComboBox;
+        private System.Windows.Forms.Label searchByNumberLabel;
+        private System.Windows.Forms.Label searchByNameLabel;
+    }
+}
+
